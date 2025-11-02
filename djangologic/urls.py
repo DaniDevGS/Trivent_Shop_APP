c479805676d3558e8aa9a9cc16771b03861a63c9
DaniDev <danieldavidsh8@gmail.com>
2025-11-02 10:00:16 -0400

    feat: Mejora en las URLS

--- djangologic/urls.py
+++ djangologic/urls.py
@@ -20,14 +20,18 @@ from products import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [
+urlpatterns = [ 
     path('admin/', admin.site.urls),
 
+    #=================================VIEWS DE Trivens======================================================
+    path('', views.index, name='index'),
+    path('tienda/', views.products_store, name='tienda'),
+    path('signup/', views.signup, name='signup'),
+    path('logout/', views.signout, name='logout'),
+    path('signin/', views.signin, name='signin'),
+    path('carrito/', views.carrito, name='carrito'),
     #=================================VIEWS DE PRODUCTS Manager======================================================
     path('manager/', views.home, name='home'),
-    path('manager/signup/', views.signup, name='signup'),
-    path('manager/logout/', views.signout, name='logout'),
-    path('manager/signin/', views.signin, name='signin'),
     path('manager/products/', views.products, name='products'),
     path('manager/products_to_send/', views.products_to_send, name='products_to_send'),
     path('manager/product/create/', views.create_product, name='create_product'),
@@ -36,10 +40,7 @@ urlpatterns = [
     path('manager/products/<int:products_id>/complete', views.sent_product, name='complete_products'), # type: ignore
     path('manager/products/<int:products_id>/delete', views.delete_product, name='delete_products'), # <-- Asegúrate de la coma # type: ignore
 
-
-    #=================================VIEWS DE Trivens======================================================
-    path('', views.index, name='index'),
-    path('tienda/', views.products_store, name='tienda')
+    
 ]
 
 if settings.DEBUG:
--- products/views.py
+++ products/views.py
@@ -31,113 +31,24 @@ def home(request):
     Returns:
         Un objeto HttpResponse que renderiza 'home.html'.
     """
-    return render(request, 'home.html')
+    return render(request, 'manager/home.html')
 
-def signup(request):
-    """Gestiona el registro de nuevos usuarios.
-
-    Si es una solicitud GET, muestra el formulario de registro.
-    Si es una solicitud POST, intenta crear un nuevo usuario y, si tiene éxito,
-    inicia la sesión del usuario y lo redirige a la lista de tareas.
-    Maneja errores si las contraseñas no coinciden o si el nombre de usuario
-    ya existe.
-
-    Args:
-        request: El objeto HttpRequest entrante.
-
-    Returns:
-        Un objeto HttpResponse que:
-        - Renderiza 'signup.html' con el formulario (GET).
-        - Redirige a 'products' (POST exitoso).
-        - Renderiza 'signup.html' con el formulario y un mensaje de error (POST fallido).
-    """
-    if request.method == 'GET':
-        return render(request, 'signup.html', {
-        'form': UserCreationForm
-        })
-    else:
-        if request.POST['password1'] == request.POST['password2']:
-            try:
-                # register user
-                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
-                user.save()
-                login(request, user)
-                return redirect('products')
-            
-            except IntegrityError:
-                return render(request, 'signup.html', {
-                    'form': UserCreationForm,
-                    'error': "User alredy exists"
-                })
-
-        return render(request, 'signup.html', {
-                    'form': UserCreationForm,
-                    'error': "Password do not match"
-                })
-
-@login_required
-def signout(request):
-    """Cierra la sesión del usuario actual y redirige a la página de inicio.
-
-    Args:
-        request: El objeto HttpRequest entrante.
-
-    Returns:
-        Un objeto HttpResponse que redirige a 'home'.
-    """
-    logout(request)
-    return redirect('home')
-
-
-def signin(request):
-    """Gestiona el inicio de sesión del usuario.
-
-    Si es una solicitud GET, muestra el formulario de autenticación.
-    Si es una solicitud POST, intenta autenticar al usuario. Si tiene éxito,
-    inicia la sesión del usuario y lo redirige a la lista de tareas. Si falla,
-    muestra un mensaje de error.
-
-    Args:
-        request: El objeto HttpRequest entrante.
-
-    Returns:
-        Un objeto HttpResponse que:
-        - Renderiza 'signin.html' con el formulario (GET).
-        - Redirige a 'products' (POST exitoso).
-        - Renderiza 'signin.html' con el formulario y un mensaje de error (POST fallido).
-    """
-
-    if request.method == 'GET':
-        return render(request, 'signin.html', {
-        'form': AuthenticationForm
-        })
-    else:
-        user = authenticate(
-            request, username=request.POST['username'], password=request.POST['password'])
-        if user is None:
-            return render(request, 'signin.html', {
-                'form': AuthenticationForm,
-                'error': 'User or password is incorrect'
-            })
-        else:
-            login(request, user)
-            return redirect('products')
 
 # =====================================================SECCION PRODUCTS===================================================================
 @login_required
 def products(request):
     productos = Producto.objects.filter(user=request.user, datecompleted__isnull=True)
-    return render(request, 'products.html', {'productos': productos , 'sent_view': False})
+    return render(request, 'manager/products.html', {'productos': productos , 'sent_view': False})
 
 @login_required
 def products_to_send(request):
     productos = Producto.objects.filter(user=request.user, datecompleted__isnull=False).order_by('-datecompleted')
-    return render(request, 'products.html', {'productos': productos, 'sent_view': True})
+    return render(request, 'manager/products.html', {'productos': productos, 'sent_view': True})
 
 @login_required
 def create_product(request):
     if request.method == 'GET':
-        return render(request, 'create_product.html', {
+        return render(request, 'manager/create_product.html', {
             'form': ProductForm
         })
     else:
@@ -149,7 +60,7 @@ def create_product(request):
             return redirect('products')
         
         except ValueError:
-            return render(request, 'create_product.html', {
+            return render(request, 'manager/create_product.html', {
                 'form': ProductForm,
                 'error': 'Porfavor provide valida data'
             })
@@ -159,7 +70,7 @@ def product_detail(request, products_id:int):
     if request.method == 'GET':
         producto = get_object_or_404(Producto, pk=products_id, user=request.user)
         form = ProductForm(instance=producto)
-        return render(request, 'products_detail.html', {'productos': producto, 'form': form})
+        return render(request, 'manager/products_detail.html', {'productos': producto, 'form': form})
     else:
         try:
             producto = get_object_or_404(Producto, pk=products_id, user=request.user)
@@ -167,7 +78,7 @@ def product_detail(request, products_id:int):
             form.save()
             return redirect('products')
         except ValueError:
-            return render(request, 'products_detail.html', {'productos': producto, 'form': form, 'error': "Error updating product"})
+            return render(request, 'manager/products_detail.html', {'productos': producto, 'form': form, 'error': "Error updating product"})
 
 def sent_product(request, products_id:int):
     # Productos enviados
@@ -207,7 +118,7 @@ def index(request):
     """
     productos = Producto.objects.filter( datecompleted__isnull=False).order_by('-datecompleted')
     
-    return render(request, 'triven/index.html', {'productos': productos, 'sent_view': True})
+    return render(request, 'index.html', {'productos': productos, 'sent_view': True})
 
 def products_store(request):
     productos = Producto.objects.filter(datecompleted__isnull=False).order_by('-datecompleted')
@@ -227,6 +138,99 @@ def products_store(request):
         else:
             # Manejo de error si la tasa no está disponible
             producto.price_ves = None # pyright: ignore[reportAttributeAccessIssue]
+    return render(request, 'tienda.html', {'productos': productos, 'sent_view': True, 'cantidad':numero_productos, 'bolivar_rate': bolivar_rate})
+
+def carrito(request):
+    return render(request, 'carrito.html')
 
 
-    return render(request, 'triven/tienda.html', {'productos': productos, 'sent_view': True, 'cantidad':numero_productos, 'bolivar_rate': bolivar_rate})
+def signup(request):
+    """Gestiona el registro de nuevos usuarios.
+
+    Si es una solicitud GET, muestra el formulario de registro.
+    Si es una solicitud POST, intenta crear un nuevo usuario y, si tiene éxito,
+    inicia la sesión del usuario y lo redirige a la lista de tareas.
+    Maneja errores si las contraseñas no coinciden o si el nombre de usuario
+    ya existe.
+
+    Args:
+        request: El objeto HttpRequest entrante.
+
+    Returns:
+        Un objeto HttpResponse que:
+        - Renderiza 'signup.html' con el formulario (GET).
+        - Redirige a 'products' (POST exitoso).
+        - Renderiza 'signup.html' con el formulario y un mensaje de error (POST fallido).
+    """
+    if request.method == 'GET':
+        return render(request, 'signup.html', {
+        'form': UserCreationForm
+        })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                # register user
+                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('tienda')
+            
+            except IntegrityError:
+                return render(request, 'signup.html', {
+                    'form': UserCreationForm,
+                    'error': "User alredy exists"
+                })
+
+        return render(request, 'signup.html', {
+                    'form': UserCreationForm,
+                    'error': "Password do not match"
+                })
+
+@login_required
+def signout(request):
+    """Cierra la sesión del usuario actual y redirige a la página de inicio.
+
+    Args:
+        request: El objeto HttpRequest entrante.
+
+    Returns:
+        Un objeto HttpResponse que redirige a 'home'.
+    """
+    logout(request)
+    return redirect('index')
+
+
+def signin(request):
+    """Gestiona el inicio de sesión del usuario.
+
+    Si es una solicitud GET, muestra el formulario de autenticación.
+    Si es una solicitud POST, intenta autenticar al usuario. Si tiene éxito,
+    inicia la sesión del usuario y lo redirige a la lista de tareas. Si falla,
+    muestra un mensaje de error.
+
+    Args:
+        request: El objeto HttpRequest entrante.
+
+    Returns:
+        Un objeto HttpResponse que:
+        - Renderiza 'signin.html' con el formulario (GET).
+        - Redirige a 'products' (POST exitoso).
+        - Renderiza 'signin.html' con el formulario y un mensaje de error (POST fallido).
+    """
+
+    if request.method == 'GET':
+        return render(request, 'signin.html', {
+        'form': AuthenticationForm
+        })
+    else:
+        user = authenticate(
+            request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'signin.html', {
+                'form': AuthenticationForm,
+                'error': 'User or password is incorrect'
+            })
+        else:
+            login(request, user)
+            return redirect('tienda')
+        
